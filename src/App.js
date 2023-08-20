f8a8fdbee59933a28f23fe4b01f96239faa32aeb
Grover Leopoldo Aliaga Candia <galiagacandia@gmail.com>
2023-08-20 14:33:26 -0400

    feat: Add the first version in html and css pure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Portfolio</h1>
+        <nav>
+          <ul>
+            <li><a href="">About Me</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </nav>
       </header>
+      <section className="profile">
+        <article className="profile-about-me">
+          <h2>I'm Grover Aliaga</h2>
+          <p>
+            A Software Developer with more than ten years of experience in Java
+            and related technologies.
+          </p>
+        </article>
+        <article>
+          <img className="profile-img" src="/photo-profile.jpg" width="400px" alt=""/>
+        </article>
+      </section>
+      <footer>
+        <section className="social">
+          <ul>
+            <li><i className="ri-github-fill"></i></li>
+            <li><i className="ri-linkedin-box-fill"></i></li>
+            <li><i className="ri-facebook-circle-fill"></i></li>
+          </ul>
+        </section>
+        <section className="copyright">@2023 Grover Aliaga</section>
+      </footer>
     </div>
   );
 }
